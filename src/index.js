f4839b9af4fe39e97831111199377f6144f841bf
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-10-30 05:49:04 +0100

    Wrap item-infos in a single Props.item

--- src/index.js
+++ src/index.js
@@ -33,38 +33,43 @@ function Examples() {
   )
 }
 
-function Item(props) {
+function Item({item}) {
+  const {title, description, photo: {src, alt}, cost} = item;
   return (
     <article>
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
-      <img width="128" alt={props.photo.alt} src={props.photo.src}/>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <img width="128" alt={alt} src={src}/>
       <hr/>
-      <button>{props.cost}</button>
+      <button>{cost}</button>
     </article>
   )
+}
+
+var housing = {
+  title: "Housing",
+  description: "Housing provided for out-of-state students or those who can't commute.",
+  photo: {
+    src: "https://upload.wikimedia.org/wikipedia/commons/9/93/Housing_Anywhere_logo.png",
+    alt: "Housing Anywhere logo"},
+  cost: "Free"
+}
+
+var fidget_spinner = {
+  title: "Fidget Spinner",
+  description: "Because we like to pretend we're in high school.",
+  photo: {
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Fidget_spinner_red%2C_cropped.jpg/512px-Fidget_spinner_red%2C_cropped.jpg",
+    alt: "Fidget spinner red, cropped"},
+  cost: "21.99$"
 }
 
 function Catalog() {
   return (
     <main>
       <h1 style={{textAlign: "center", textTransform: "uppercase"}}>Catalog</h1>
-      <Item
-        title="Housing"
-        description="Housing provided for out-of-state students or those who can't commute."
-        photo={{
-          src: "https://upload.wikimedia.org/wikipedia/commons/9/93/Housing_Anywhere_logo.png",
-          alt: "Housing Anywhere logo"}}
-        cost="Free"
-      />
-      <Item
-        title="Fidget Spinner"
-        description="Because we like to pretend we're in high school."
-        photo={{
-          src: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Fidget_spinner_red%2C_cropped.jpg/512px-Fidget_spinner_red%2C_cropped.jpg",
-          alt: "Fidget spinner red, cropped"}}
-        cost="21.99$"
-      />
+      <Item item={housing} />
+      <Item item={fidget_spinner} />
     </main>
   )
 }
