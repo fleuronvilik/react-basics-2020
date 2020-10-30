791201f73204c3741a337c594853e6ed6a9472a6
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-10-30 02:56:33 +0100

    Define EXAMPLES that renders a Description List

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const EXAMPLES = (
+  <dl>
+    <dt className="c">C</dt>
+    <dd>
+      <code>printf("Hello, World!\n");</code>
+    </dd>
+    <dt className="py">Python</dt> {/*; not required*/}
+    <dd>
+      <code>print("Hello, World!")</code>
+    </dd>
+    <dt className="js">JavaScript</dt>
+    <dd>
+      <code>console.log("Hello, World!")</code>
+    </dd>
+  </dl>
+);
+
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
+  EXAMPLES,
   document.getElementById('root')
 );
