f04a122e1e8fe2d85573878bd406f72d73fce949
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-10-30 02:59:36 +0100

    Turn EXAMPLES into a Stateless Functional Component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const EXAMPLES = (
-  <dl>
-    <dt className="c">C</dt>
-    <dd>
-      <code>printf("Hello, World!\n");</code>
-    </dd>
-    <dt className="py">Python</dt> {/*; not required*/}
-    <dd>
-      <code>print("Hello, World!")</code>
-    </dd>
-    <dt className="js">JavaScript</dt>
-    <dd>
-      <code>console.log("Hello, World!")</code>
-    </dd>
-  </dl>
-);
+function Examples() {
+  return (
+    <dl>
+      <dt className="c">C</dt>
+      <dd>
+        <code>printf("Hello, World!\n");</code>
+      </dd>
+      <dt className="py">Python</dt> {/*; not required*/}
+      <dd>
+        <code>print("Hello, World!")</code>
+      </dd>
+      <dt className="js">JavaScript</dt>
+      <dd>
+        <code>console.log("Hello, World!")</code>
+      </dd>
+    </dl>
+  );
+}
 
 ReactDOM.render(
-  EXAMPLES,
+  <Examples />,
   document.getElementById('root')
 );
