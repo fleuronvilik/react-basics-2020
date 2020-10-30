59f3599d2f1f14b8ecdf492e0aa483c2087ca2e0
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-10-30 05:06:11 +0100

    Pass Props to Item so that items can be different

--- src/index.js
+++ src/index.js
@@ -33,14 +33,14 @@ function Examples() {
   )
 }
 
-function Item() {
+function Item(props) {
   return (
     <article>
-      <h2>Housing</h2>
-      <p>Housing provided for out-of-state students or those who can't commute.</p>
-      <img width="128" alt="Housing Anywhere logo" src="https://upload.wikimedia.org/wikipedia/commons/9/93/Housing_Anywhere_logo.png"/>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <img width="128" alt={props.photo.alt} src={props.photo.src}/>
       <hr/>
-      <button>Free</button>
+      <button>{props.cost}</button>
     </article>
   )
 }
@@ -49,8 +49,22 @@ function Catalog() {
   return (
     <main>
       <h1 style={{textAlign: "center", textTransform: "uppercase"}}>Catalog</h1>
-      <Item />
-      <Item />
+      <Item
+        title="Housing"
+        description="Housing provided for out-of-state students or those who can't commute."
+        photo={{
+          src: "https://upload.wikimedia.org/wikipedia/commons/9/93/Housing_Anywhere_logo.png",
+          alt: "Housing Anywhere logo"}}
+        cost="Free"
+      />
+      <Item
+        title="Fidget Spinner"
+        description="Because we like to pretend we're in high school."
+        photo={{
+          src: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Fidget_spinner_red%2C_cropped.jpg/512px-Fidget_spinner_red%2C_cropped.jpg",
+          alt: "Fidget spinner red, cropped"}}
+        cost="21.99$"
+      />
     </main>
   )
 }
